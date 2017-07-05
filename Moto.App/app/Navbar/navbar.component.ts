204a3e161bb7604bb410bcb1cb17050d88e2bee3
Paweł Płaza <pawelekplaza@gmail.com>
2017-07-05 23:01:18 +0200

    Conflict with navbar.

---
+++ Moto.App/app/Navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'navbar-cmp',
+    templateUrl: 'app/Navbar/navbar.component.html'
+})
+
+export class NavbarComponent {
+    name: string = "Motocykle";
+}
